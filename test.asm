1f9c549adf2df81070bd2d26f37bbf035561e2a1
PuruSinghvi <97847091+PuruSinghvi@users.noreply.github.com>
2022-11-13 10:14:09 +0530

    Add files via upload

---
+++ test.asm
@@ -0,0 +1,10 @@
+add $8, $9, $10  #tesst
+hello: add $19, $16, $17
+sub $8, $9, $10
+or $8, $9, $10
+addi $8, $9, 0x5 # random
+addi $19, $16, 10
+ori $8, $9, 5 #hello
+sll $s2,$t3, 2
+lw $t0, 4($s3)
+lui $s2, -2
